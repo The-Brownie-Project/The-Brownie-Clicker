96f8b1aba1d6f6d5bbf9a476cbbe4a39b73d8013
5p3c7r4l <enderspectral69150@gmail.com>
2024-03-12 22:15:10 +0100

    *JS*
    Creation of branch JS
    
    Added 4 new files:
    -brownie.js
    -config.js (empty)
    -functionalities.js
    -variables.js
    
    Added OOP methods in functionalities
    
    TODO:put in relation brownie.js with the rest and rename it

---
+++ TheBrownieClicker/src/scripts/functionalities.js
@@ -0,0 +1,90 @@
+/* All the usefull functions the program needs */
+
+/* IT IS IMPORTANT THAT ALL FUNCTIONS AND CLASSES ARE PLACED BELOW */
+
+import variables from "./variables";
+
+class Init{
+    constructor(brownies,scoreDisplay,autoClickButton) {
+        this.brownies = brownies;
+        this.scoreDisplay = scoreDisplay;
+        this.autoClickButton = autoClickButton;
+    }
+    
+    constructor(brownies,scoreDisplay){
+        this.brownies = brownies;
+        this.scoreDisplay = scoreDisplay;
+    }
+};
+
+class Items{
+    constructor(Clickable,Collectable,Bonus,CanSpawn){
+        this.Clickable = Clickable;
+        this.Collectable = Collectable;
+        this.Bonus = Bonus;
+        this.CanSpawn = CanSpawn;
+    };
+
+    spawnable(CanSpawn){
+        if (CanSpawn)
+        {
+            return this.Collectable = true;
+        }
+        else
+        {
+            return this.Collectable = false;
+        }
+    }
+
+    collactable(Collectable,Clickable){
+        //ayayaya
+        var fuckyouaxel = new Array(); 
+
+        switch (Clickable)
+        {
+            case true : {
+                UnableOnClick = true;
+                fuckyouaxel.push(UnableOnClick)
+            };
+            case false : {
+                UnableOnClick = false;
+                fuckyouaxel.push(UnableOnClick)
+            };
+            default : {
+                UnableOnClick = null;
+                fuckyouaxel.push(UnableOnClick)
+            };
+        }
+        switch (Collectable)
+        {
+            case true : {
+                HasBonus = true;
+                fuckyouaxel.push(HasBonus);
+            }
+            case false : {
+                HasBonus = false;
+                fuckyouaxel.push(HasBonus);
+            }
+            default : {
+                HasBonus = null;
+                fuckyouaxel.push(HasBonus);
+            }
+        }
+    return fuckyouaxel // :ok_hand:
+    }
+};
+
+/* 
+WARNING:This Class's only purpose is to serve as a placeholder for future items
+        Do not use it in the main program outside of testing
+*/
+class DummyItem extends Items {
+    constructor(Clickable,Collectable,Bonus,CanSpawn,HasBonus){
+        super(Clickable,Collectable,Bonus,CanSpawn);
+        this.HasBonus = HasBonus;
+    }
+
+    ItemProperties(HasBonus){
+
+    }
+};
---
+++ TheBrownieClicker/src/scripts/variables.js
@@ -0,0 +1,41 @@
+/* list of all the values used in the BrownieClicker game
+
+########################################################
+#                  ALL RIGHT RESERVED                  #
+#                                                      #
+#                                                      #
+#                                                      # 
+########################################################
+
+
+*/
+
+
+
+/* All Int variables */
+
+var Bonus       = Int8Array();
+var score       = BigInt(0);
+
+/*###*/
+
+
+
+/* All boolean variables */
+
+var Clickable       = Boolean();
+var Collectable     = Boolean();
+var CanSpawn        = Boolean();
+var UnableOnClick   = Boolean();
+
+/*###*/
+
+
+/* All non default type variables */
+
+var brownie         = document.querySelector("#brownie");
+var scoreDisplay    = document.querySelector("#score-display");
+var autoClickButton = document.querySelector("#autoclickbutton");
+var clickableBonus  = document.querySelector("#clickablebonus")
+    
+/*###*/
